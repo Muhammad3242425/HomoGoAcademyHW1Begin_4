4b5a72ea27ddb68c575a1d4624eec8ba1cd5432e
BOBOEV <Muhammad3242425>
2023-11-10 12:42:27 +0500

    add files4

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a float64
+	
+	fmt.Print("a = ")
+	fmt.Scan(&a)
+
+	fmt.Println("V =", math.Pow(a,3))
+	fmt.Println("S =", 6*(math.Pow(a,2)))
+
+}
